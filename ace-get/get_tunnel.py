4359e8732517306176176c25c162ba35e5e1f6d1
Vova Khrystenko <ovod88@bigmir.net>
2018-07-15 13:01:33 -0700

    Remoce ace-get script from tracking

--- ace-get/get_tunnel.py
+++
@@ -1,8 +0,0 @@
-def open_tunnel(uname,passwd,local_port,target_ip,target_port,jumphost_ip,jumphost_port):
-	return SSHTunnelForwarder(
-		(jumphost_ip, jumphost_port), # jumpserver
-		ssh_username=uname,
-		ssh_password=passwd,
-		remote_bind_address=(target_ip, target_port), # target device
-		local_bind_address=('0.0.0.0', local_port) # local port
-	)
